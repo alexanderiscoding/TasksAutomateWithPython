98233e9ad285809203ac9d9e724b67a4917ea181
Alexander <94754185+alexanderfsm@users.noreply.github.com>
2022-06-05 17:31:13 -0300

    Create MergeJsonAllOnlyFile.py

---
+++ MergeJsonAllOnlyFile.py
@@ -0,0 +1,30 @@
+#python MergeJsonAllOnlyFile.py example1.json example2.json... infinite files in only file
+import json
+import sys
+
+filename = sys.argv[1].rsplit('.', 1)[0]
+convertedfilename = "%s_merged_all.json" % (filename)
+try:
+    with open(convertedfilename) as file:
+        print("Já existe um arquivo de destino, favor renomear ou apagar.")
+except FileNotFoundError:
+    all_files = sys.argv[1:]
+    for item in all_files:
+        try:
+            with open(convertedfilename, encoding="utf-8") as file:
+                data_all = json.loads(file.read())
+                with open(item, encoding="utf-8") as file:
+                    data = json.loads(file.read())
+                    with open(convertedfilename, 'w', encoding='utf8') as outfile:
+                        json.dump({**data_all, **data}, outfile, sort_keys = True, indent = 4, ensure_ascii = False)
+        except FileNotFoundError:
+            with open(item, encoding="utf-8") as file:
+                data = json.loads(file.read())
+                with open(convertedfilename, 'w', encoding='utf8') as outfile:
+                        json.dump(data, outfile, sort_keys = True, indent = 4, ensure_ascii = False)
+        except json.decoder.JSONDecodeError:
+            print("O arquivo %s pode não ser um .json" % item)
+        except Exception as e:
+            print("Ocorreu um erro:\n", e)
+except Exception as e:
+    print("Ocorreu um erro:\n", e)
